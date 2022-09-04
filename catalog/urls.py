326e2c93895ce62f9e51797e791794a03657d148
Ismael <ismael@3bweb.com>
2022-09-04 19:46:35 +0100

    Add code

---
+++ catalog/urls.py
@@ -0,0 +1,37 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('books/', views.BookListView.as_view(), name='books'),
+    # <int:pk> allows us to capture the book id
+    # We can use re_path if we want to use regex
+    path('book/<int:pk>', views.BookDetailView.as_view(), name='book-detail'),
+
+    path('authors/', views.AuthorListView.as_view(), name='authors'),
+    path('author/<int:pk>', views.AuthorDetailView.as_view(), name='author-detail'),
+
+    path('mybooks/', views.LoanedBookByUserListView.as_view(), name="my-borrowed"),
+
+    path('borrowedbooks/', views.AllLoanedBookListView.as_view(), name="all-borrowed"),
+    path('book/<uuid:pk>/renew/', views.renew_book_librarian,
+         name='renew-book-librarian'),
+
+    path('author/create/',
+         views.AuthorCreate.as_view(), name="author_create"),
+
+    path('author/<int:pk>/update/',
+         views.AuthorUpdate.as_view(), name="author_update"),
+
+    path('author/<int:pk>/delete/',
+         views.AuthorDelete.as_view(), name="author_delete"),
+
+    path('book/create/',
+         views.BookCreate.as_view(), name="book_create"),
+
+    path('book/<int:pk>/update/',
+         views.BookUpdate.as_view(), name="book_update"),
+
+    path('book/<int:pk>/delete/',
+         views.BookDelete.as_view(), name="book_delete"),
+]
---
+++ catalog/views.py
@@ -0,0 +1,153 @@
+from catalog.models import Author
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from django.shortcuts import render
+from catalog.models import Book, Author, BookInstance, Genre, Language
+from django.views import generic
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
+
+import datetime
+from django.contrib.auth.decorators import login_required, permission_required
+from django.shortcuts import get_object_or_404
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+
+from catalog.forms import RenewBookForm
+
+
+def index(request):
+    # View function for home page
+
+    # Count number of books and instances
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+
+    # Available books (Those with status = 'a')
+    # We can filter a search with attribute__exact = 'value'
+    num_instances_available = BookInstance.objects.filter(
+        status__exact='a').count()
+
+    # All in author is implied
+    num_authors = Author.objects.count()
+
+    num_genres = Genre.objects.all().count()
+
+    num_languages = Language.objects.all().count()
+
+    num_visits = request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits + 1
+
+    context = {
+        'num_books': num_books,
+        'num_instances': num_instances,
+        'num_instances_available': num_instances_available,
+        'num_authors': num_authors,
+        'num_genres': num_genres,
+        'num_languages': num_languages,
+        'num_visits': num_visits,
+    }
+
+    # Render the HTML template index.html with the data in the context
+    return render(request, 'index.html', context=context)
+
+
+class BookListView(generic.ListView):
+    # This view will actually be implemented as a class. We will be inheriting from an existing generic view function that already does most of what we want this view function to do, rather than writing our own from scratch.
+    model = Book
+    paginate_by = 5
+
+
+class BookDetailView(generic.DetailView):
+    model = Book
+
+
+class AuthorListView(generic.ListView):
+    model = Author
+    paginate_by = 5
+
+
+class AuthorDetailView(generic.DetailView):
+    model = Author
+
+
+class LoanedBookByUserListView(LoginRequiredMixin, generic.ListView):
+    # List of the books on loan from the current user
+    model = BookInstance
+    template_name = "catalog/bookinstance_list_borrowed_user.html"
+    paginate_by = 5
+
+    def get_queryset(self):
+        return BookInstance.objects.filter(borrower=self.request.user).filter(status__exact='o').order_by('due_back')
+
+
+class AllLoanedBookListView(LoginRequiredMixin, generic.ListView):
+    # List of the books on loan from the current user
+    model = BookInstance
+    template_name = "catalog/bookinstance_list_borrowed_all.html"
+    paginate_by = 5
+
+    def get_queryset(self):
+        return BookInstance.objects.filter(status__exact='o').order_by('due_back')
+
+
+@login_required
+@permission_required('catalog.can_mark_returned', raise_exception=True)
+def renew_book_librarian(request, pk):
+    # View function for renewing a bookinstance
+    book_instance = get_object_or_404(BookInstance, pk=pk)
+
+    if request.method == 'POST':
+        form = RenewBookForm(request.POST)
+
+        if form.is_valid():
+            book_instance.due_back = form.cleaned_data['renewal_date']
+            book_instance.save()
+            return HttpResponseRedirect(reverse('all-borrowed'))
+    else:
+        proposed_renewal_date = datetime.date.today() + datetime.timedelta(weeks=3)
+        form = RenewBookForm(initial={'renewal_date': proposed_renewal_date})
+
+    context = {
+        'form': form,
+        'book_instance': book_instance,
+    }
+
+    return render(request, 'catalog/book_renew_librarian.html', context)
+
+
+class AuthorCreate(PermissionRequiredMixin, CreateView):
+    permission_required = 'catalog.can_mark_returned'
+    model = Author
+    fields = '__all__'
+    initial = {'date_of_death':  datetime.date.today()}
+
+
+class AuthorUpdate(PermissionRequiredMixin, UpdateView):
+    permission_required = 'catalog.can_mark_returned'
+    model = Author
+    fields = ['first_name', 'last_name', 'date_of_birth', 'date_of_death']
+
+
+class AuthorDelete(PermissionRequiredMixin, DeleteView):
+    permission_required = 'catalog.can_mark_returned'
+    model = Author
+    success_url = reverse_lazy('authors')
+
+
+# Book creating, update and delete
+class BookCreate(PermissionRequiredMixin, CreateView):
+    permission_required = 'catalog.can_mark_returned'
+    model = Book
+    fields = '__all__'
+
+
+class BookUpdate(PermissionRequiredMixin, UpdateView):
+    permission_required = 'catalog.can_mark_returned'
+    model = Book
+    fields = ['title', 'author', 'summary', 'isbn', 'genre', 'language']
+
+
+class BookDelete(PermissionRequiredMixin, DeleteView):
+    permission_required = 'catalog.can_mark_returned'
+    model = Book
+    success_url = reverse_lazy('books')
